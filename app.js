5eadd118791287d1b8004d945784ef3d4d8205ef
Lindsey Lawler <lawlerlindsey@gmail.com>
2022-12-02 17:50:44 -0500

    AuthRoute finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
+const AuthRoute = require('./routes/AuthRoute')
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -11,7 +12,7 @@ app.use(logger('dev'))
 app.use(express.json())
 
 //ROUTES GO HERE
-
+app.use('/auth', AuthRoute)
 
 
 app.listen(PORT, () => { 
